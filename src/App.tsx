b34b20d28e942f5fd6385e567a0e4323fb0bf2aa
edurodrigues0 <edusacramento9@gmail.com>
2022-08-26 13:14:09 -0300

    js to tsx conversion from Input Component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import GlobalStyle from './styles/global';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
+
+export function App() {
+  return(
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  )
+}
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,21 @@
 import {
+  ElementType,
+  useCallback,
   useEffect,
   useRef,
   useState,
-  useCallback,
 } from 'react';
 
+import { Container } from './styles';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+interface InputProps {
+  name: string;
+  icon: ElementType;
+}
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
